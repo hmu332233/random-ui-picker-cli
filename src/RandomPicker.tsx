fcfa60191c71d89f9afe11d734b680cbef695215
minung.han <hmu332233@gmail.com>
2021-06-26 23:55:59 +0900

    init

---
+++ src/RandomPicker.tsx
@@ -0,0 +1,13 @@
+  
+import React from "react";
+import { Box, Text } from "ink";
+
+ function RandomPicker() {
+  return (
+    <Box margin={1} padding={2} flexDirection="column">
+      <Text>테스트</Text>
+    </Box>
+  )
+};
+
+export default RandomPicker;
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import RandomPicker from './RandomPicker';
+import { render } from 'ink';
+
+render(<RandomPicker />);
