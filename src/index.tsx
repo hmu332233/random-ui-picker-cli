1e4f570e07927bda4d749ba058c3de8b7f57757b
minung.han <hmu332233@gmail.com>
2021-07-31 17:18:32 +0900

    feat: Add theme options

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import RandomPicker from './RandomPicker';
+import Basic from './Basic';
 import { render } from 'ink';
 
 import { Command } from 'commander';
 
 import * as fs from 'fs';
 
+const ThemeMap: PickerThemeMap = {
+  basic: Basic,
+};
+
 const program = new Command();
 
 program
+  .option('-t --theme <theme>', 'theme, [default: basic]')
   .option('-i --items <items...>', 'items')
   .option('-if --items_file <path>', 'items\'s file path')
-  .action(({ items, items_file }) => {
+  .action(({ theme = 'basic', items, items_file }) => {
     
     if (items_file) {
       items = fs.readFileSync(items_file, 'utf8').split('\n');
     }
 
-    render(<RandomPicker items={items} />);
+    const Picker = ThemeMap[theme] || Basic;
+
+    render(<Picker items={items} />);
   });
 
 program.parse();
